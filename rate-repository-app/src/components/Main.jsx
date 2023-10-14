7fd953820fb5b0d2aabebab633983699bf3cab7d
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-10-14 19:30:06 +0200

    Remove top margin in the main view.

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,10 +1,9 @@
-import {StatusBar, StyleSheet, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import AppBar from "./AppBar/AppBar";
 import RepositoryList from "./RepositoryList";
 
 const styles = StyleSheet.create({
     main: {
-        marginTop: StatusBar.currentHeight || 0,
         flexGrow: 1,
         flexShrink: 1,
         backgroundColor: "#e1e4e8",
