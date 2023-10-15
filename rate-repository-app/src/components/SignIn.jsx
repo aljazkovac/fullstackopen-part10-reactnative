fe34c154b99cb6ca4da2acaab8cdda92f879bcbf
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-10-15 22:00:21 +0200

    Start implementing SignIn form. WIP. Needs styling.

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,35 @@
+import { View, Pressable } from "react-native";
+import { Formik } from "formik";
 import Text from "./Basic/Text";
+import FormikTextInput from "./Basic/FormikTextInput";
 
-const SignIn = () => {
-    return <Text>The sign-in view</Text>;
+const onSubmit = (values) => {
+    console.log(values);
 };
 
+const SignInForm = ({ onSubmit }) => {
+    return (
+        <View>
+            <FormikTextInput name="username" placeholder="username" />
+            <FormikTextInput name="password" placeholder="password" secureTextEntry />
+            <Pressable onPress={onSubmit}>
+                <Text>Submit</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+const SignIn = () => {
+    return (
+        <Formik
+            initialValues={{
+                username: "",
+                password: "",
+            }}
+            onSubmit={onSubmit}
+        >
+            {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+        </Formik>
+    );
+}
 export default SignIn;
