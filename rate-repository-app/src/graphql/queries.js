bdff3d19e979f68040bb8228397973a14488b85a
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2023-11-09 18:03:56 +0100

    Create GetRepository query and useRepository hook.

--- rate-repository-app/src/graphql/queries.js
+++ rate-repository-app/src/graphql/queries.js
@@ -30,4 +30,38 @@ export const GET_USER = gql`
             username
         }
     }
-    `;
+`;
+
+export const GET_REPOSITORY = gql`
+    query($id: ID!) {
+        repository(id: $id) {
+            id
+            name
+            ownerName
+            createdAt
+            fullName
+            reviewCount
+            ratingAverage
+            forksCount
+            stargazersCount
+            description
+            language
+            ownerAvatarUrl
+            url
+            reviews {
+                edges {
+                    node {
+                        id
+                        text
+                        rating
+                        createdAt
+                        user {
+                            id
+                            username
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
